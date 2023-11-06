45cfa03a6a4771715f7dfee64f3ab01cd1391fb1
Jhon Santos <kennedyalves871@gmail.com>
2023-11-05 21:42:27 -0300

    Introdução ao React

---
+++ src/Button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FunctionComponent, ReactNode } from "react";
+
+interface ButtonProps {
+    text?: string;
+    onClick?: () => void;
+    children?: ReactNode;
+}
+
+const Button: FunctionComponent<ButtonProps> = ({ text, onClick }) => {
+    return <div onClick={onClick}>{text}</div>;
+};
+
+export default Button;
